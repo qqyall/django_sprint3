c5b484430c284de046ec55c9eca8c86bba15626b
Румянцев Савелий <radogask9rsd@gmail.com>
2024-01-23 16:40:11 +0500

    PEP8 fixes

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -16,7 +16,8 @@ class Category(PublishedModel, CreatedAtModel):
         unique=True,
         blank=False,
         verbose_name="Идентификатор",
-        help_text="Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание."
+        help_text=("Идентификатор страницы для URL; "
+                   "разрешены символы латиницы, цифры, дефис и подчёркивание.")
     )
     is_published = models.BooleanField(
         default=True,
@@ -60,7 +61,8 @@ class Post(PublishedModel, CreatedAtModel):
     pub_date = models.DateTimeField(
         blank=False,
         verbose_name=("Дата и время публикации"),
-        help_text="Если установить дату и время в будущем — можно делать отложенные публикации."
+        help_text=("Если установить дату и время в будущем "
+                   "— можно делать отложенные публикации.")
     )
     author = models.ForeignKey(
         User,
